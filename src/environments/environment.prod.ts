97ca32a9197857a4a493dcde736bc1b9e6115eee
hady mohamed <hady812012@gmail.com>
2025-09-22 03:32:33 +0300

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   API_VERSION: 'v1',
-  API_URL: `https://api.azalove.com`,
+  API_URL: `https://api.azalove.com/api`,
   Domain_URL: `https://admin.azalove.com`,
 };
